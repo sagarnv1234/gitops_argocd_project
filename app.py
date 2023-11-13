f3af1982880c243d67a0ef3f42816d79dbbd5a6a
sagarnv1234 <nelas.vit@gmail.com>
2023-11-13 13:42:52 +0530

    change the h1 tag

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ def health():
 
 @app.route("/")
 def home():
-    return "Welcome to Mr DevOps Youtube Channel"
+    return "Hi this is my projecct hosted on aws which includes jennkins , docker , kubenates , argocd"
    
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int("5000"), debug=True)
